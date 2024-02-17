824bd4866714c0951cc775b73d0171807616ce97
Brunella Quaye <131973976+brunellaquaye@users.noreply.github.com>
2024-02-17 19:24:27 +0000

    feat:Add file to convert temperatures between celcuis and fahrenheit.

---
+++ temperatureConverter.js
@@ -0,0 +1,16 @@
+//Develop a function that converts temperatures between Celsius and Fahrenheit. Write a function that takes a temperature value and a unit (Celsius or Fahrenheit) as input and returns the converted temperature.
+function converter(value,unit){
+    if (unit==="c"||"C"){
+        console.log(value*(9/5)+32)
+    }if (unit === "f"||"F" ) {
+        console.log((value - 32) * (5/9))
+    }if (unit === "k"||"K") {
+        console.log()
+    }
+    
+    else{
+        console.log("The temperature you provided does not have the right values")
+    }
+}
+const currentTemperature = converter(29,"c");
+//Add a feature to accept input from user later.
