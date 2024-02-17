5083db1df5de62bf2f16a65b59e3d4102e543fb8
Brunella Quaye <131973976+brunellaquaye@users.noreply.github.com>
2024-02-17 20:20:54 +0000

    Update : Update file to handle uppercase version of the units.

--- temperatureConverter.js
+++ temperatureConverter.js
@@ -1,14 +1,15 @@
 //Develop a function that converts temperatures between Celsius and Fahrenheit. Write a function that takes a temperature value and a unit (Celsius or Fahrenheit) as input and returns the converted temperature.
 function converter(value,unit){
-    if (unit === "c"){
+    //The "||" allows for both cases of units("C" and "c") to be allowed,enhancing code functionality.
+    if ((unit === "c") || (unit === "C" )) {
         console.log("The temperature is : " + (((9/5)*value)+ 32) + " fahrenheit.")
-    }else if (unit === "f") {
+    }else if ((unit === "f") || (unit === "F")) {
         console.log("The temperature is:"+((value - 32) * (5/9))+ " celcius.");
     }
     else{
         console.log("This unit for measurement cannot be processed.")
     }
 }
-const currentTemperature = converter(25,"c");
+const currentTemperature = converter(25,"F");
 //Add a feature to accept input from user later.
 //Add feature to convert between more units of measurement of temperature.
